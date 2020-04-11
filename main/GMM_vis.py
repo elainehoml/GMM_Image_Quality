0e5b89537a80d296062c220564d14b6b62ce7aa2
emlh1n13 <eml.ho@soton.ac.uk>
2020-04-11 23:36:02 +0100

    Adds simple Bokeh visualisation for GMM plots
    - Used GMM_vis conda environment
    - Bokeh plot in html under results directory
    - Requires image filename for an image to which GMMs have already been
      fitted
    
    Future plans
    ------------
    - Raise error if GMMs not fitted (check if results_dir exists)
    - Hover over a Gaussian to check out its mu, sigma and weight
    - Add FileInput option to choose input image filename
    - Add second panel showing thresholded image

---
+++ main/GMM_vis.py
@@ -0,0 +1,69 @@
+# Script for bokeh visualisation of GMM results
+
+# Imports and housekeeping
+import os
+import numpy as np
+from scipy.stats import norm
+from bokeh.layouts import gridplot
+from bokeh.plotting import figure, output_file, show
+from bokeh.palettes import Set2 as palette
+import QM_load
+
+# Functions
+
+def bokeh_plot(img_fname):
+    """ Generates Bokeh plot of img histogram and fitted GMM results
+    Parameters
+    ----------
+
+    Returns
+    -------
+
+    """
+    # Create plot
+    basename = os.path.splitext(os.path.basename(img_fname))[0]
+    p = figure(title="Image: {}, click on legend entries to hide".format(basename))
+    #TODO[Elaine]: Figure out hover to show mu and sigma
+    #TODO[Elaine]: Figure out how to use ColumnDataSource
+    #TODO[Elaine]: Choose image from file input to view
+
+    # Import image and generate histogram CDS
+    img = QM_load.QM_load(img_fname)
+    hist, edges = np.histogram(img, density=True, bins=int(0.25 * np.sqrt(len(img))))
+    bin_width = edges[1] - edges[0]
+    bin_midpoint = edges[:-1] + bin_width
+    
+    # Histogram
+    p.vbar(x=bin_midpoint, top=hist, bottom=0, width=bin_width, alpha=0.5, legend_label="Image histogram")
+    p.xaxis.axis_label = "Grey values"
+    p.yaxis.axis_label = "Probability Density"
+
+    # GMM results
+    results_dir = "{}_results".format(os.path.splitext(img_fname)[0])
+    GMM_results = np.loadtxt(os.path.join(results_dir, "fitted_results.csv"), delimiter=",")
+    norm_mat = np.zeros([len(bin_midpoint), len(GMM_results)])
+    colours = (palette[8])
+    for i in range(len(GMM_results)): # for each Gaussian
+        mu = GMM_results[i,0]
+        sigma = GMM_results[i,1]
+        weight = GMM_results[i,2]
+        y_norm = norm.pdf(bin_midpoint, mu, sigma) # y coordinates if area under dist = 1
+        norm_mat[:,i] = y_norm * weight # y coordinates if weights are applied
+        p.varea(x=bin_midpoint, y1=np.zeros(len(bin_midpoint)), y2=y_norm, alpha=0.3, color=colours[i], legend_label="Gaussian {}".format(str(i)))
+        p.line(x=bin_midpoint, y=y_norm, line_color=colours[i], legend_label="Gaussian {}".format(str(i)))
+    p.line(x=bin_midpoint, y=np.sum(norm_mat, axis=1), line_color="black", line_dash="dashed", legend_label="Sum of fitted Gaussians") # Sum of fitted Gaussians
+
+    # Legend
+    p.legend.click_policy = "hide"
+
+    # Show
+    output_file(os.path.join(results_dir, "{}_GMM_vis.html".format(basename)), title="{} GMM fit".format(basename))
+    show(p)
+    
+    return p
+
+# Main
+if __name__ == "__main__": 
+    test_fname = os.path.join(os.path.dirname(os.getcwd()), "test", "test.tif")
+    img_basename = os.path.basename(test_fname)
+    p = bokeh_plot(test_fname)
